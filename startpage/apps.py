9848734cbc073df424cc435cf50cae0b3e46193b
givinity <SaZh.user@gmail.com>
2023-01-21 23:21:45 +0300

    add commit

--- startpage/apps.py
+++ startpage/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class StartpageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'startpage'
+    verbose_name = 'Женщины мира'
