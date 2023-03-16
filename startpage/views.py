2021ba294e1e0c151b0b508a0cfcc24d0941290e
givinity <SaZh.user@gmail.com>
2023-03-16 14:41:09 +0300

    Optimized SQL-requests

--- startpage/views.py
+++ startpage/views.py
@@ -25,7 +25,7 @@ class WomenHome(DataMixin, ListView):
         context = dict(list(context.items()) + list(c_def.items()))
         return context
     def get_queryset(self):
-        return Women.objects.filter(is_published=True)
+        return Women.objects.filter(is_published=True).select_related('cat')
 
 #def index(request):
 #    posts = Women.objects.all()
@@ -65,12 +65,13 @@ class WomenCategory(DataMixin, ListView):
 
     def get_queryset(self):
         return Women.objects.filter(cat__slug=self.kwargs['cat_slug'],
-                                    is_published=True)
+                                    is_published=True).select_related('cat')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title='Категория -' + str(context['posts'][0].cat),
-                                      cat_selected = context['posts'][0].cat_id)
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title='Категория -' + str(c.name),
+                                      cat_selected = c.name)
 
         return dict(list(context.items()) + list(c_def.items()))
         # context['title'] = 'Категория -' + str(context['posts'][0].cat)
